b364eeb8c8ab4d159463ed26fa216503426380b8
jratul <jratul@nate.com>
2024-07-23 19:50:11 +0900

    add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express, { Express } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import "dotenv/config";
 
 const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Seoul Food Server");
