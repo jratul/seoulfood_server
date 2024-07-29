30958283b66431d49d38172402108aa2cd8a9a4d
jratul <jratul@nate.com>
2024-07-29 19:52:43 +0900

    add limit for getFoods

--- src/controllers/foods.controller.ts
+++ src/controllers/foods.controller.ts
@@ -4,6 +4,7 @@ import { Request, Response, NextFunction } from "express";
 export const getFoods = (req: Request, res: Response, next: NextFunction) => {
   foodModel
     .find({})
+    .limit(12)
     .then((foods) => {
       res.status(200).json(foods);
     })
