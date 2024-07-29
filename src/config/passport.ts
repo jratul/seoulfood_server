d5aa949ae0a6f2a843a63e33e7241480307878a5
jratul <jratul@nate.com>
2024-07-29 20:23:51 +0900

    fix google strategy callback url

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -5,7 +5,7 @@ const googleStrategyConfig = new GoogleStrategy.Strategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID || "",
     clientSecret: process.env.GOOGLE_CLIENT_PW || "",
-    callbackURL: "/users/google/callback",
+    callbackURL: "https://sfserver.jratul.shop:9876/users/google/callback",
     scope: ["email", "profile"],
   },
   (_accessToken, _refreshToken, profile, done) => {
